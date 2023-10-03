b4be60e4b194ebaad0dc238200933cda199e1ddf
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-03 17:43:21 +0300

    Update LetsTalk.js

--- etc-front/src/components/pages/LetsTalk.js
+++ etc-front/src/components/pages/LetsTalk.js
@@ -1,26 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './LetsTalk.css'
 function LetsTalk() {
   return (
     <>
    
-    <div  className='lets-talk2'>
-  
+    <div  className='lets-talk3'>
+    
     <div style={{marginTop:'-300px'}}className='lets-talk-content'>
       <p style={{fontSize:'16px',textAlign:'left',paddingLeft:'46px'}} className='lets'>LET'S TALK</p>
       <h1 style={{fontSize:'42px',textAlign:'left', paddingLeft:'15px', color:'#fff'}}>Need our products?</h1>
-      <hr
-        style={{
-          width: '100%',
-          marginTop: '20px',
-          marginBottom: '20px',
-          background: '#ededed',
-          color: '#ededed',
-          borderColor: '#ededed',
-          height: '1px'
-        }}
-      />
-      <p style={{marginTop:'40px',fontSize:'16px', paddingLeft:'50px'}} className='pro'>We’re a team of professionals who are excited to bring healthy and affordable eggs and meat to every Ethiopian family.</p>
+ 
+      <p style={{marginTop:'40px',fontSize:'16px', paddingLeft:'50px',marginBottom:'-30px'}} className='pro'>We’re a team of professionals who are excited to bring healthy and affordable eggs and meat to every Ethiopian family.</p>
+     <Link to={'/sign-up'}>  <button class="contact-button">Contact</button></Link>
     </div>
     </div>
     </>

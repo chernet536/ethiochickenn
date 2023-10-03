5eaad8ab7491104c650f9efc42c40808dc84711d
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-03 18:14:16 +0300

    Update FeedContent.js

--- etc-front/src/components/pages/FeedContent.js
+++ etc-front/src/components/pages/FeedContent.js
@@ -3,25 +3,54 @@ import './FeedContent.css';
 function FeedContent() {
   return (
     <>
+    <div className='Poultry-container'>
     <div className='Poultry-Feed'>
       <h2 className='Poultry-title'>Poultry Feed</h2>
       <p>We provide a full range of formulated poultry feed from the best quality ingredients. In addition, our customized poultry diets address each breed's specific needs to ensure optimum health, growth, and overall quality during the different stages of growth or requirement. Our main products are:</p>
     </div>
+    </div>
     <div className='container'>
       <div className='paragraph1'>
          <h1 className='starter'>Starter</h1>
-         <p>Commercial Starter feed is a high protein chicken feed prepared to meet the dietary requirements of day-old chicks. The layer or dual-purpose chicken should be fed starter feed for the first six weeks of their life before progressing onto grower feed. The high protein content helps young chicks grow fast becoming good pullets.</p> 
+         <p>A High protein chicken feed prepared to meet the dietary requirements of day-old chicks. The layer or dual-purpose chicken should be fed starter feed for the first six weeks of their life before progressing onto grower feed. The high protein content helps young chicks grow fast and develop into hardy pullets.</p> 
       </div>
       <div className='paragraph2'>
-         <h1 className='grower'>Grower</h1>
-         <p>Commercial Grower feed is designed to fulfill the dietary requirements of a chicken between the ages of 6 to 20 weeks. Grower feed contains a protein content between 16-18% but has less calcium than regular layer feed.</p> 
+         <h1 className='grower'>Premium Starter</h1>
+         <p>Expertly crafted to nourish your day-old chicks for their first crucial weeks. Packed with essential proteins and nutrients, it fosters rapid growth and robust health, setting the stage for strong, productive pullets or dual-purpose chickens. As your chicks mature, this feed eases their transition to grower feed, ensuring a smooth developmental path.  </p> 
       </div>
       <div className='paragraph3'>
-         <h1 className='layer'>Layer</h1>
-         <p>Commercial layer feed contains a balance of protein, calcium, and other vitamins and minerals that encourage consistent egg-laying abilities in the flock. Layer feed has extra calcium to ensure that the eggshells are strong leading to less breakages. Layer feed should only be fed to chickens from 20 weeks of age or once they have started to lay eggs. Before the start of feeding layer feed, mix half layer and half grower feed from week 16 till the point of lay, then switch to 100%-layer feed.
+         <h1 className='layer'>Grower</h1>
+         <p>Designed to fulfill the dietary requirements of a chicken between the ages of 6 to 20 weeks. 
 </p> 
       </div>
     </div>
+    <div className='container'>
+      <div className='paragraph1'>
+         <h1 className='starter'>Pre-lay</h1>
+         <p>Prepare your chickens for peak egg-laying with our specially designed feed for those aged 16 to 20 weeks. It bridges the gap between grower and layer feed, offering balanced nutrition with added calcium for strong eggshells. Transitioning is effortless - blend with grower feed from week 16 till laying starts. </p> 
+      </div>
+      <div className='paragraph2'>
+         <h1 className='grower'>Layer</h1>
+         <p>Commercial Layer feeds are for chickens laying eggs, supplying all the necessary nutrition to support them during their productive life.</p> 
+      </div>
+      <div className='paragraph3'>
+         <h1 className='layer'>Broiler Starter</h1>
+         <p>Specifically formulated for day-old chicks, this high-protein feed provides the essential nutrients needed for a strong and healthy beginning. With the perfect blend of proteins and vitamins, it fosters rapid growth and development, setting the stage for robust broilers. 
+</p> 
+      </div>
+    </div>
+    <div className='container'>
+      <div className='paragraph1'>
+         <h1 className='starter'>Broiler Grower</h1>
+         <p>Nurturing your birds into adolescence is a critical step. As your broilers advance in age, our Broiler Grower Feed ensures their continued growth and vitality. It maintains the right balance of nutrients to support healthy and robust development with excess protein. </p> 
+      </div>
+      <div className='paragraph2'>
+         <h1 className='grower'>Broiler Finisher</h1>
+         <p>As your chickens approach market weight, our Finisher Feed is your ticket to top-quality results. The final stage of your broiler's journey is where quality truly shines. Our Broiler Finisher Feed is expertly crafted to provide the optimal nutrients for efficient muscle development and ideal finishing. </p> 
+      </div>
+      
+    </div>
+  
     </>
   )
 }

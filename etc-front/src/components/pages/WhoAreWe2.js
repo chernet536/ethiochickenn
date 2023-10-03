05cf166209db899fe6c44b62ccb56c79a4f164d7
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-03 18:17:37 +0300

    Update WhoAreWe2.js

--- etc-front/src/components/pages/WhoAreWe2.js
+++ etc-front/src/components/pages/WhoAreWe2.js
@@ -4,20 +4,12 @@ function WhoAreWe2() {
   return (
     <>
     <div  className='lets-talk2'>
-    <div style={{marginTop:'-200px'}}className='lets-talk-content'>
-      <h1 style={{fontSize:'42px' , textAlign:'left',paddingLeft:'16px', color:'#fff'}}>Who are we?</h1>
-      <hr
-        style={{
-          width: '100%',
-          marginTop: '20px',
-          marginBottom: '20px',
-          background: '#ededed',
-          color: '#ededed',
-          borderColor: '#ededed',
-          height: '1px'
-        }}
-      />
-      <p style={{marginTop:'40px',fontSize:'16px', paddingLeft:'50px'}} className='pro'>EthioChicken launched in 2010 with it's breeder farm, feed mill and hatchery</p>
+    <div className='lets-talk-content'>
+      <div className='who-are-we-container'>
+      <h1 className = 'who-are-we-title'>Who are we?</h1>
+   
+      <p className = 'who-are-we-content'>EthioChicken launched in 2010 with it's breeder farm, feed mill and hatchery</p>
+      </div>
     </div>
     </div>
     </>

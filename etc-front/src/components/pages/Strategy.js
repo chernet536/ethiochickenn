9c57bbd95467ab35cce0d3f8e089ed00765cb48d
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-04 11:47:38 +0300

    Update Strategy.js

--- etc-front/src/components/pages/Strategy.js
+++ etc-front/src/components/pages/Strategy.js
@@ -5,8 +5,9 @@ function Strategy() {
     <>
     <div  className='lets-talk5'>
     <div style={{marginTop:'100px'}}className='lets-talk-content'>
-      <h1 style={{marginTop:'80px',textAlign:'left',fontSize:'42px', paddingLeft:'16px', color:'#fff'}}>Strategy</h1>
-      <p style={{color:'#fff', marginTop:'80px',fontSize:'16px', paddingLeft:'50px'}} className='pro'></p>
+      <div className='strategy-container'>
+        <h1 style={{marginTop:'80px',textAlign:'left',fontSize:'42px', paddingLeft:'16px', color:'#fff'}}>Strategy</h1>
+      </div>
     </div>
     </div>
     </>

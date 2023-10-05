97f3039361554d7545c6a9f2ea261d94ca87d24f
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-05 17:25:49 +0300

    Update DayChickContent.js

--- etc-front/src/components/pages/DayChickContent.js
+++ etc-front/src/components/pages/DayChickContent.js
@@ -22,12 +22,7 @@ function DayChickContent() {
          <p>A commercial layer purely for commercial egg production under controlled egg production infrastructure with formulated feeding regimes only.</p>
        </div>
       
-       <div className='paragraph7'>
-          <img className='Lohmann'src={Lohmann} alt='no-image' />
-          <h1 className='lohmann-title'>Lohmann Brown</h1>
-          <p>A popular and highly productive commercial breed of laying hens. These hens are specifically bred for their egg-laying abilities and are known for their exceptional egg production.</p>
-
-       </div>
+      
     </div>
     </>
   )

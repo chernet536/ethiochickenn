8367d56ed3c49ed2287b858b79f41c8ebf794611
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-05 10:54:42 +0300

    Update WhoAreWe.jsx

--- etc-front/src/components/pages/WhoAreWe.jsx
+++ etc-front/src/components/pages/WhoAreWe.jsx
@@ -4,32 +4,8 @@ function WhoAreWe() {
   return (
     <div
       className='who-main'
-      style={{
-        marginTop:'-140px',
-        marginBottom:'10px',
-        marginLeft:'auto',
-        marginRight:'auto',
-        width: '60%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        height: '100vh',
-        '@media (max-width: 768px)': {
-          width: '90%',
-          marginTop: '0',
-          height: 'auto'
-        },
-        '@media (max-width: 480px)': {
-          width: '100%',
-          marginTop: '0',
-          height: 'auto',
-          padding: '10px'
-        }
-      }}
     >
-      <h1 style={{ color: '#8a181b' }}>ABOUT US</h1>
+      <h1 className='about-us-title'>ABOUT US</h1>
       <hr
         style={{
           width: '100%',

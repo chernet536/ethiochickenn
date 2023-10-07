edf08aa09711e03fbd28a9a2b6da8e3b852fc994
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-07 08:23:40 +0300

    Update Teams.js

--- etc-front/src/components/pages/Teams.js
+++ etc-front/src/components/pages/Teams.js
@@ -11,50 +11,50 @@ import DR_Mulualem from '../../images/Mulualem.jpg';
 function Teams() {
   return (
     <div className='Teams'>
-    <h1 style={{textAlign:'center', marginTop:'30px'}}>OUR TEAMS</h1>
+    <h1 style={{textAlign:'center', marginTop:'30px'}}>OUR TEAM</h1>
     <div className='image-collection2'>
        <div className='image7' data-text="">
            <img src={Justin} alt='dayoldchick' className='teams-images'/>
-           <h3 style={{}}>DR. JUSTIN BENADE</h3>
+           <h3 style={{}}>DR. Justin Benade</h3>
            <p>Ethiopia Managing Director</p>
        </div>
        <div className='image8' data-text="">
            <img src={Joseph} alt='dayoldchick' className='teams-images'/>
-           <h3  style={{}}>JOSEPH CHIVINGE</h3>
+           <h3  style={{}}>Mr. Joseph Chivinge</h3>
            <p>Finance Director</p>
       </div>
       <div className='image9' data-text="">
            <img src={Mulugeta} alt='dayoldchick' className='teams-images' />
-           <h3  style={{}}>MULUGETA ABRHA</h3>
-           <p>Feed Business Director</p>
+           <h3  style={{}}>Mr. Mulugeta Abrha</h3>
+           <p>Exports Director</p>
       </div>
     </div>
     <div className='image-collection2'>
        <div className='image7' data-text="">
            <img src={DR_Mulualem} alt='dayoldchick' className='teams-images'/>
-           <h3 style={{}}>DR.Mulualem Tesfaye</h3>
+           <h3 style={{}}>DR. Mulualem Tesfaye</h3>
            <p>Sales Director</p>
        </div>
        <div className='image8' data-text="">
            <img src={DR_Fikresilassie} alt='dayoldchick' className='teams-images' />
-           <h3  style={{}}>DR.FIKRESILASSIE DAWIT</h3>
+           <h3  style={{}}>DR. Fikresilassie Dawit</h3>
            <p>Production Director</p>
       </div>
       <div className='image9'  data-text="">
            <img src={Yonatan} alt='dayoldchick' className='teams-images'/>
-           <h3  style={{}}>YONATAN FSEHA</h3>
+           <h3  style={{}}>Mr. Yonatan Fseha</h3>
            <p>Human Resource Director</p>
       </div>
     </div>
     <div className='image-collection2'>
        <div className='image7' data-text="">
            <img src={Makeda} alt='dayoldchick' className='teams-images'/>
-           <h3 style={{}}>Makeda Mekonnen</h3>
+           <h3 style={{}}>Ms. Makeda Mekonnen</h3>
            <p>Chief of Staff</p>
        </div>
        <div className='image8' data-text="">
            <img src={Sean} alt='dayoldchick' className='teams-images' />
-           <h3  style={{}}>Sean Moran</h3>
+           <h3  style={{}}>Mr. Sean Moran</h3>
            <p>Supply Chain and Logistics Director</p>
       </div>
       

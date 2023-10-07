d51d3949dcf81435c447c6e20a5b122ab5c914f7
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-07 07:26:29 +0300

    Update VisionMission.js

--- etc-front/src/components/pages/VisionMission.js
+++ etc-front/src/components/pages/VisionMission.js
@@ -5,10 +5,10 @@ function VisionMission() {
     <div className='vision-image-container'>
       <div style={{marginTop:'-60px'}}className='vision-mission'>
         <h1 className='VisionTitle'>VISION</h1>
-        <p style={{paddingLeft:'10px',fontSize:'16px', color:'#8a181b'}}>To provide one chicken per person per year through smallholder farmers.</p>
+        <p style={{paddingLeft:'10px',fontSize:'16px', color:'#8a181b'}}>To provide one chicken per person per year through smallholder farmers</p>
         <h1 className='MissionTitle'>MISSION</h1>
         <p style={{paddingLeft:'10px', fontSize:'16px', color:'#8a181b'}}>To bring healthy and affordable eggs and meat to every Ethiopian family, and in doing so 
-          improve nutrition, enhance rural farmer livelihoods, and create income opportunities for our customers and partners.
+          improve nutrition, enhance rural farmer livelihoods, and create income opportunities for our customers and partners
            </p>
            <h1 className='MissionTitle'>PURPOSE</h1>
         <p style={{paddingLeft:'10px', fontSize:'16px', color:'#8a181b'}}>

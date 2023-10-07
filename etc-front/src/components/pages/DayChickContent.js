c69ede45ddbe3afb72d4a38e32a135ab91552a16
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-07 08:02:17 +0300

    Update DayChickContent.js

--- etc-front/src/components/pages/DayChickContent.js
+++ etc-front/src/components/pages/DayChickContent.js
@@ -7,19 +7,19 @@ function DayChickContent() {
   return (
     <>
     <div className='day-chick-explain'>
-      <h1>Day Old Chicken</h1>
+      <h1>Day Old Chicks</h1>
       <p> We provide a diverse range of day-old chicks. Our commitment to quality begins with <br></br> meticulous breeding and extends to rigorous grading, selection, vaccination, and biosecurity protocols <br></br> to ensure our customers are 100% satisfied.</p>
     </div>
     <div className='container8'>
        <div className='paragraph5'>
          <img className='sasso'src={sasso} alt='no-image' />
           <h1 className='sasso-title'>Sasso T451</h1>
-          <p>Dual purpose chicken that can be used for both egg and meat production under scavenger/supplement feeding environments.</p>
+          <p>Dual purpose chicken that can be used for both egg and meat production under scavenger/supplement feeding environments</p>
        </div>
        <div className='paragraph6'>
          <img className='BrownEgg'src={BrownEgg} alt='no-image' />
          <h1 className='Brown-title'>Brown Egg Layer Chicken</h1>
-         <p>A commercial layer purely for commercial egg production under controlled egg production infrastructure with formulated feeding regimes only.</p>
+         <p>A commercial egg layer chicken which performs best under commercial poultry farming conditions with complete feeding</p>
        </div>
       
       

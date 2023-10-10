13dc188a2172ac40ffce93641a0f58441611d5af
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-10-10 10:33:25 +0300

    Update OurPartners.jsx

--- etc-front/src/components/pages/OurPartners.jsx
+++ etc-front/src/components/pages/OurPartners.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Bill from '../../images/billgatesnew.png';
-import ifc from '../../images/ifcnew.png';
-import finnfund from '../../images/Logo_finnfund.jpg';
-import Hendrix from '../../images/HenNew.png';
-import nutreco from '../../images/NutrecoNew.png';
-import zoetis from '../../images/ZoetisNew.png';
+import Bill from '../../images/Untitled-1Artboard-1.png';
+import ifc from '../../images/Untitled-1Artboard-1-copy-2.png';
+import finnfund from '../../images/Untitled-1Artboard-1-copy.png';
+import Hendrix from '../../images/Untitled-1Artboard-1-copy-3.png';
+import nutreco from '../../images/Untitled-1Artboard-1-copy-5.png';
+import zoetis from '../../images/Untitled-1Artboard-1-copy-4.png';
 import './OurPartners.css';
 function OurPartners() {
   return (
@@ -17,19 +17,19 @@ function OurPartners() {
             <img className='our-partner-images'  src={Bill} alt='dayoldchick'  />
         </div>
         <div className='img-container'>
-            <img className='our-partner-images' src={ifc} alt='dayoldchick'  />
+            <img className='our-partner-images' src={finnfund} alt='dayoldchick'  />
         </div>
         <div className='img-container'>
-            <img className='our-partner-images'  src={finnfund} alt='dayoldchick'  />
+            <img className='our-partner-images'  src={ifc} alt='dayoldchick'  />
         </div>
         <div className='img-container'>
             <img className='our-partner-images'  src={Hendrix} alt='dayoldchick' />
         </div>
         <div className='img-container'>
-            <img className='our-partner-images'  src={nutreco} alt='dayoldchick' />
+            <img className='our-partner-images'  src={zoetis} alt='dayoldchick' />
         </div>
         <div className='img-container'>
-            <img className='our-partner-images'  src={zoetis} alt='dayoldchick' />
+            <img className='our-partner-images'  src={nutreco} alt='dayoldchick' />
         </div>
       </div>
     </div>

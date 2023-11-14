ab6dc983c63a2ab99a926aca2c0e79d2a43ca913
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-11-14 05:25:43 +0300

    Update LoginPage.js

--- etc-front/src/components/pages/LoginPage.js
+++ etc-front/src/components/pages/LoginPage.js
@@ -61,11 +61,11 @@ export default function LoginPage() {
 
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch(`${process.env.REACT_APP_URL}/login`, {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
+      credentials:"include"
     });
     if (response.ok) {
       response.json().then(userInfo => {
@@ -78,7 +78,7 @@ export default function LoginPage() {
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />;
+    return <Navigate to={'/create'} />;
   }
 
   return (

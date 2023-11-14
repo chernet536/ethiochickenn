71cb9c8430d4b64bd7309f5df0b090dd7f8539df
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-11-14 05:38:22 +0300

    Update SignUp.js

--- etc-front/src/components/pages/SignUp.js
+++ etc-front/src/components/pages/SignUp.js
@@ -5,6 +5,7 @@ import MapComponent from './MapComponent';
 import Footer from './Footer2'
 export default function SignUp() {
   return <>
+   <div style={{marginTop:'100px'}}></div>
     <ContactForm />
     <MapComponent />
     <Footer />

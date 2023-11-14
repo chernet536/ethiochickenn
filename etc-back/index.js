822dd6bc826341477c034892118ca4fd15dcbece
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-11-14 05:47:25 +0300

    Update index.js

--- etc-back/index.js
+++ etc-back/index.js
@@ -17,7 +17,10 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 const secret2 = 'asfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({
+  origin:true,
+  credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
